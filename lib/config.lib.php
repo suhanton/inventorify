55119234291f77f7cf87f8218d9a444f8e6ce0b5
williamlang <william@jadedpixel.com>
2010-05-13 10:47:29 -0400

    1.0

--- lib/config.lib.php
+++ lib/config.lib.php
@@ -7,5 +7,5 @@
 	//This is the path to your installation (without trailing slash!)
 	//If you are installed at http://www.yourdomain.com/myApp then
 	//that is used as the path.
-	define('HTTP_PATH', 'http://localhost/~william/inventory_count');
+	define('HTTP_PATH', 'http://localhost/~william/inventorify');
 ?>
