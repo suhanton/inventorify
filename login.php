a15dccd5bb14e8c10e7f2de3a68c80474ff867c1
williamlang <william@jadedpixel.com>
2010-05-17 16:39:16 -0400

    Fixed a bug in login.php to allow logged in users access to the app without relogging in

--- login.php
+++ login.php
@@ -5,28 +5,33 @@
 	include('lib/shopify_api_config.php');
 	include('lib/shopify_api.php');
 	if (!defined('API_KEY') || !defined('SECRET') || isEmpty(API_KEY) || isEmpty(SECRET)) die('Both constants API_KEY and SECRET must be defined in the config file.');
-
-	if (isset($_GET['t'])){
-	  $_SESSION['url'] = $_GET['shop'];
-	  $_SESSION['token'] = $_GET['t'];
-	  $_SESSION['timestamp'] = $_GET['timestamp'];
-	  $_SESSION['signature'] = $_GET['signature'];
-
-		if (isset($_SESSION['nextAction'])){
-			$nextAction = $_SESSION['nextAction'];
-			unset($_SESSION['nextAction']);					
-			header("Location: index.php?action=" . $nextAction);
-		}else{
-			header("Location: index.php");
-		}
+	
+	if (isset($_SESSION['url'])){
+    $action = (isset($_GET['action'])) ? $_GET['action'] : 'inventory';
+    header("Location: index.php?action=" . $action);
 	}else{
-		if (isset($_GET['shop'])){
-			$url = $_GET['shop'];
-			$_SESSION['nextAction'] = (isset($_GET['action'])) ? $_GET['action'] : 'inventory';
-			$api = new Session($url, '', API_KEY, SECRET);
-			header("Location: " . $api->create_permission_url());
-		}else{
-			header("Location: index.php?action=authorize");
-		}
+	  if (isset($_GET['shop'])){
+	    if (isset($_GET['t'])){
+  	    $_SESSION['url'] = $_GET['shop'];
+    	  $_SESSION['token'] = $_GET['t'];
+    	  $_SESSION['timestamp'] = $_GET['timestamp'];
+    	  $_SESSION['signature'] = $_GET['signature'];
+
+    		if (isset($_SESSION['nextAction'])){
+    			$nextAction = $_SESSION['nextAction'];
+    			unset($_SESSION['nextAction']);					
+    			header("Location: index.php?action=" . $nextAction);
+    		}else{
+    			header("Location: index.php");
+    		}	      
+	    }else{
+  			$url = $_GET['shop'];
+  			$_SESSION['nextAction'] = (isset($_GET['action'])) ? $_GET['action'] : 'inventory';
+  			$api = new Session($url, '', API_KEY, SECRET);
+  			header("Location: " . $api->create_permission_url());
+	    }
+	  }else{
+      header("Location: index.php?action=authorize");	    
+	  }
 	}
 ?>
